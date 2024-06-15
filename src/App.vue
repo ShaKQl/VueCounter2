19e78152094f0f9a544d4fc2b13cb596c5fd7c9b
ShaKQl <halimovsvhzod@gmail.com>
2024-06-15 12:45:57 +0500

    Main Structure Made

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
-  <div>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
 
-  </div>
+      <button @click="addCount()">+</button>
+      <button @click="subtractCount">-</button>
+    </div>
+  </main>
 </template>
 
 <script setup>
+  import { ref } from "vue";
+  let count = ref(0) 
 
+  const addCount = ()=> {
+    count.value += 1
+  }
+  const subtractCount = ()=> {
+    count.value -= 1
+  }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+  main{
+    background: aliceblue;
+    width: 100vw;
+    height: 100vh; 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
+  div{
+    text-align: center;
+  }
 </style>
