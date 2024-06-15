649bd26277ba7c9c2751b715eabcf5b99ed7211d
ShaKQl <halimovsvhzod@gmail.com>
2024-06-15 14:39:32 +0500

    Button_Style_Changed

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,87 @@
 <template>
   <main>
-    <div>
-      <h4>The current count is...</h4>
-      <h1>{{ count }}</h1>
+    <div class="display">
+      <div class="display__case--zero">
+        <h4>The current count is...</h4>
+        <h1>{{ count }}</h1>
+      </div>
 
-      <button @click="addCount()">+</button>
-      <button @click="subtractCount">-</button>
+      <div class="display__case--positive">
+        <h4>The current count is {{ count }}</h4>
+      </div>
+
+      <div class="button__wrapper">
+        <button class="button" @click="addCount(), checkForCount">+</button>
+        <button class="button" @click="subtractCount(), checkForCount">-</button>
+      </div>
     </div>
   </main>
 </template>
 
+
+
+
+
+
+
 <script setup>
   import { ref } from "vue";
-  let count = ref(0) 
+  let count = ref(0)
 
-  const addCount = ()=> {
+  const addCount = () => {
     count.value += 1
   }
-  const subtractCount = ()=> {
-    count.value -= 1
+  const subtractCount = () => {
+    if (count.value == 0) {
+      return count.value
+    }
+    else {
+      count.value -= 1
+    }
   }
 </script>
 
-<style scoped>
-  main{
-    background: aliceblue;
-    width: 100vw;
-    height: 100vh; 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
 
-  div{
-    text-align: center;
-  }
+
+
+
+
+
+<style scoped>
+main {
+  background: aliceblue;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  text-align: center;
+}
+
+.button {
+  width: 150px;
+  width: 100%;
+  border-radius: 25px;
+  background-color: #858585;
+  border: none;
+  transition: 0.5s ease-in-out;
+}
+
+.button__wrapper {
+  display: flex;
+  gap: 15px;
+  margin-top: 15px;
+}
+
+.button:hover {
+  background-color: #393939;
+  color: #fff;
+}
+
+.display__case--positive{
+  display: none;
+}
 </style>
